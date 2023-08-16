182d6ae4951e648cc7fdf535e4c1a32df4a752df
Reezo1986 <Reezo1986@gmail.com>
2023-08-16 08:01:57 +0600

    Heap sorting

---
+++ Heap_sort.py
@@ -0,0 +1,34 @@
+def heapify(arr, n, i):
+    largest = i
+    left = 2 * i + 1
+    right = 2 * i + 2
+
+    if left < n and arr[left] > arr[largest]:
+        largest = left
+
+    if right < n and arr[right] > arr[largest]:
+        largest = right
+
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        heapify(arr, n, largest)
+
+def heap_sort(arr):
+    n = len(arr)
+
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    for i in range(n - 1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+        heapify(arr, i, 0)
+
+    return arr
+
+array = [2, 11, 13, 5, 10, 7]
+print("Исходный массив:")
+print(array)
+
+sorted_array = heap_sort(array.copy())
+print("Отсортированный массив:")
+print(sorted_array)
